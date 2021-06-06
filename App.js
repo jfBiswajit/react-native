f38199bed2ab4fa147872f02e8e9868932e08e87
Biswajit Biswas <jfbiswajit@gmail.com>
2021-06-06 17:29:33 +0600

    Add: made some changes

--- App.js
+++ App.js
@@ -3,9 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const name = "Biswajit Biswas";
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontSize: 45 }}>Getting started with react native</Text>
+      <Text style={{ fontSize: 20 }}>Hello there!</Text>
+      <Text>{name}</Text>
       <StatusBar style="auto" />
     </View>
   );
