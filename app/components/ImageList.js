cc5177c2ffebb5db72e70a77e9711867a344cdbe
Biswajit Biswas <jfbiswajit@gmail.com>
2021-06-07 17:11:35 +0600

    Add: image list component added

---
+++ app/components/ImageList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+
+const ImageList = (pros) => {
+  return <Text>Imgae List</Text>;
+};
+
+export default ImageList;
--- app/screens/ImageScreen.js
+++ app/screens/ImageScreen.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import ImageList from '../components/ImageList'
 
 const ImageScreen = (pros) => {
-  return <Text>Image</Text>;
+  return (
+    <View>
+      <ImageList/>
+      <ImageList/>
+      <ImageList/>
+      <ImageList/>
+    </View>
+  )
 };
 
 export default ImageScreen;
