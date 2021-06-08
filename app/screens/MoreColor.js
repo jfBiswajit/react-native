9a87b1be6ed504ea35bb3f42e08db91f390c406f
Biswajit Biswas <jfbiswajit@gmail.com>
2021-06-08 12:48:02 +0600

    Add: wokrs as expected

--- app/screens/MoreColor.js
+++ app/screens/MoreColor.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import ColorCounter from '../components/ColorCounter';
 
 const MoreColor = () => {
-
   return (
     <View>
-      <Text>More Color</Text>
+      <ColorCounter color="Red" />
+      <ColorCounter color="Green" />
+      <ColorCounter color="Blue" />
     </View>
   );
 };
