8b818d4302db07c7e64897a26c0a5f656552ea12
Biswajit Biswas <jfbiswajit@gmail.com>
2021-06-08 17:05:21 +0600

    Add: text input added

--- app/screens/BoxModelScreen.js
+++ app/screens/BoxModelScreen.js
@@ -3,12 +3,24 @@ import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 
 const BoxModelScreen = () => {
   return (
-    <View>
-      <Text>Box Model</Text>
+    <View style={style.view}>
+      <Text style={style.text}>Box Model</Text>
     </View>
   );
 };
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  view: {
+    borderWidth: 5,
+    borderColor: 'black',
+    padding: 10
+  },
+  text: {
+    borderWidth: 2,
+    borderColor: 'red',
+    textAlign: 'center',
+    padding: 5
+  }
+});
 
 export default BoxModelScreen;
