3db93e667c7674626762de24267e8f42e8e19dc7
Biswajit Biswas <jfbiswajit@gmail.com>
2021-06-08 17:39:48 +0600

    Add: text input added

--- app/screens/BoxModelScreen.js
+++ app/screens/BoxModelScreen.js
@@ -3,12 +3,46 @@ import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 
 const BoxModelScreen = () => {
   return (
-    <View style={style.view}>
-      <Text style={style.text}>Box Model</Text>
-      <View style={style.container}>
-        <Text style={[style.item, { backgroundColor: '#3498db' }, {alignSelf: 'flex-end'}]}>Item One</Text>
-        <Text style={[style.item, { backgroundColor: '#27ae60' },{alignSelf: 'center'}]}>Item Two</Text>
-        <Text style={[style.item, { backgroundColor: '#d35400' }, {alignSelf: 'flex-start'}]}>Item Three</Text>
+    <View>
+      <View style={style.view}>
+        <Text style={style.text}>Flex Box</Text>
+        <View style={style.container}>
+          <Text
+            style={[
+              style.item,
+              { backgroundColor: '#3498db' },
+              { alignSelf: 'flex-end' },
+            ]}
+          >
+            Item One
+          </Text>
+          <Text
+            style={[
+              style.item,
+              { backgroundColor: '#27ae60' },
+              { alignSelf: 'center' },
+            ]}
+          >
+            Item Two
+          </Text>
+          <Text
+            style={[
+              style.item,
+              { backgroundColor: '#d35400' },
+              { alignSelf: 'flex-start' },
+            ]}
+          >
+            Item Three
+          </Text>
+        </View>
+      </View>
+      <View style={style.view}>
+        <Text style={style.text}>Position</Text>
+        <View style={style.container}>
+          <View style={style.parent}>
+            <View style={style.child}></View>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -27,13 +61,25 @@ const style = StyleSheet.create({
     padding: 5,
   },
   container: {
-    alignItems: 'stretch'
+    alignItems: 'stretch',
   },
   item: {
     padding: 20,
     marginBottom: 10,
-    textAlign: 'center'
-  }
+    textAlign: 'center',
+  },
+  parent: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'red',
+    position: 'relative',
+  },
+  child: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'green',
+    top: '30%',
+  },
 });
 
 export default BoxModelScreen;
